f80139785259c719ea00983c4fb2607132a19e81
Luca Bettinelli <luca.bettinelli92@gmail.com>
2023-07-18 23:13:55 +0200

    Add some style Add URL Shortener save and retrieve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import {createBrowserRouter, RouterProvider, useLoaderData} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function App() {
 
@@ -11,8 +12,7 @@ function App() {
                 return null;
             }
 
-            const result = await resp.json();
-            return result;
+            return await resp.json();
         } catch (ex) {
             console.log(ex);
             return null;
@@ -32,19 +32,109 @@ function App() {
     ])
 
     return (
-        <>
+        <div className="layout">
             <RouterProvider router={router}/>
-        </>
+        </div>
     )
 }
 
 function ShortenerForm() {
+    const [url, setUrl] = useState("");
+    const [shorten, setShorten] = useState(null);
+    const [toastVisible, setToastVisibility] = useState(false);
+
+    const addUrl = async (url) => {
+        if (!url || url === "") {
+            setToastVisibility(true);
+            return;
+        }
+        try {
+            const resp = await fetch(`${window.config.basePath}/api/url/`,
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        url: url
+                    })
+                });
+
+            if (resp.status > 299) {
+                return null;
+            }
+
+            const result = await resp.json();
+            setShorten(result.url_short)
+        } catch (ex) {
+            console.log(ex);
+            return null;
+        }
+    }
+
+    useEffect(() => {
+        if (toastVisible) {
+            const timeout = setTimeout(() => {
+                setToastVisibility(false)
+            }, 2000);
+            return () => {
+                clearTimeout(timeout);
+            }
+        }
+    }, [toastVisible])
+
+    if (shorten) {
+        const completeUrlShort = `${window.location.protocol}//${window.location.host}/${shorten}`
+        return (
+            <>
+                <div>
+                    <h1
+                        onClick={() => {
+                            navigator.clipboard.writeText(completeUrlShort)
+                                .then(() => {
+                                    setToastVisibility(true);
+                                })
+                        }}
+                    >
+                        {completeUrlShort}
+                    </h1>
+                    <button
+                        onClick={() => {
+                            setShorten(null);
+                            setUrl("")
+                        }}>
+                        Back
+                    </button>
+                </div>
+                {toastVisible &&
+                    <div className="toast__container">
+                        <div className="toast">Url copied!</div>
+                    </div>
+                }</>
+        )
+    }
+
     return (
-        <div>
-            <span>URL:</span>
-            <input type="text"/>
-            <button>Shorten URL</button>
-        </div>
+        <>
+            <div className="shortener__container">
+                <span>URL:</span>
+                <input type="text"
+                       value={url}
+                       onChange={(e) => setUrl(e.target.value)}
+                       onKeyUp={(e) => {
+                           if (e.key === "Enter") {
+                               addUrl(url);
+                           }
+                       }}
+                />
+                <button onClick={() => addUrl(url)}>Shorten URL</button>
+            </div>
+            {toastVisible &&
+                <div className="toast__container">
+                    <div className="toast toast--err">No url inserted!</div>
+                </div>
+            }
+        </>
     )
 }
 
